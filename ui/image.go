db524fb2e78da9fc4341f2c27ae7ac5363902e8c
delthas <delthas@dille.cc>
2024-07-29 15:10:29 +0200

    Add image EXIF rotation support
    Forgot to git add :-)

---
+++ ui/image.go
@@ -0,0 +1,153 @@
+package ui
+
+import (
+	"bytes"
+	"encoding/binary"
+	"image"
+	"io"
+
+	"github.com/disintegration/imaging"
+)
+
+func exifOrientation(r io.Reader) int {
+	// Inspired by: https://github.com/disintegration/imageorient
+
+	const (
+		markerSOI      = 0xffd8
+		markerAPP1     = 0xffe1
+		exifHeader     = 0x45786966
+		byteOrderBE    = 0x4d4d
+		byteOrderLE    = 0x4949
+		orientationTag = 0x0112
+	)
+
+	// Check if JPEG SOI marker is present.
+	var soi uint16
+	if err := binary.Read(r, binary.BigEndian, &soi); err != nil {
+		return 0
+	}
+	if soi != markerSOI {
+		return 0 // Missing JPEG SOI marker.
+	}
+
+	// Find JPEG APP1 marker.
+	for {
+		var marker, size uint16
+		if err := binary.Read(r, binary.BigEndian, &marker); err != nil {
+			return 0
+		}
+		if err := binary.Read(r, binary.BigEndian, &size); err != nil {
+			return 0
+		}
+		if marker>>8 != 0xff {
+			return 0 // Invalid JPEG marker.
+		}
+		if marker == markerAPP1 {
+			break
+		}
+		if size < 2 {
+			return 0 // Invalid block size.
+		}
+		if _, err := io.CopyN(io.Discard, r, int64(size-2)); err != nil {
+			return 0
+		}
+	}
+
+	// Check if EXIF header is present.
+	var header uint32
+	if err := binary.Read(r, binary.BigEndian, &header); err != nil {
+		return 0
+	}
+	if header != exifHeader {
+		return 0
+	}
+	if _, err := io.CopyN(io.Discard, r, 2); err != nil {
+		return 0
+	}
+
+	// Read byte order information.
+	var (
+		byteOrderTag uint16
+		byteOrder    binary.ByteOrder
+	)
+	if err := binary.Read(r, binary.BigEndian, &byteOrderTag); err != nil {
+		return 0
+	}
+	switch byteOrderTag {
+	case byteOrderBE:
+		byteOrder = binary.BigEndian
+	case byteOrderLE:
+		byteOrder = binary.LittleEndian
+	default:
+		return 0 // Invalid byte order flag.
+	}
+	if _, err := io.CopyN(io.Discard, r, 2); err != nil {
+		return 0
+	}
+
+	// Skip the EXIF offset.
+	var offset uint32
+	if err := binary.Read(r, byteOrder, &offset); err != nil {
+		return 0
+	}
+	if offset < 8 {
+		return 0 // Invalid offset value.
+	}
+	if _, err := io.CopyN(io.Discard, r, int64(offset-8)); err != nil {
+		return 0
+	}
+
+	// Read the number of tags.
+	var numTags uint16
+	if err := binary.Read(r, byteOrder, &numTags); err != nil {
+		return 0
+	}
+
+	// Find the orientation tag.
+	for i := 0; i < int(numTags); i++ {
+		var tag uint16
+		if err := binary.Read(r, byteOrder, &tag); err != nil {
+			return 0
+		}
+		if tag != orientationTag {
+			if _, err := io.CopyN(io.Discard, r, 10); err != nil {
+				return 0
+			}
+			continue
+		}
+		if _, err := io.CopyN(io.Discard, r, 6); err != nil {
+			return 0
+		}
+		var val uint16
+		if err := binary.Read(r, byteOrder, &val); err != nil {
+			return 0
+		}
+		if val < 1 || val > 8 {
+			return 0 // Invalid tag value.
+		}
+		return int(val)
+	}
+	return 0 // Missing orientation tag.
+}
+
+func DecodeImage(r io.Reader) (image.Image, string, error) {
+	var b bytes.Buffer
+	tr := io.TeeReader(io.LimitReader(r, 1<<20), &b)
+	o := exifOrientation(tr)
+	r = io.MultiReader(&b, r)
+
+	img, format, err := image.Decode(r)
+	if err != nil {
+		return img, format, err
+	}
+
+	switch o {
+	case 3:
+		img = imaging.Rotate180(img)
+	case 6:
+		img = imaging.Rotate270(img)
+	case 8:
+		img = imaging.Rotate90(img)
+	}
+	return img, format, nil
+}
